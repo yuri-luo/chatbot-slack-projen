22541b8f0d8010e1bad3e386c6ac1101f29e58de
yuri-luo <yuri.luo@104.com.tw>
2021-06-11 21:02:06 +0800

    remove bin

--- bin/chatbot-slack-demo.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { ChatbotSlackDemoStack } from '../src/chatbot-slack-demo-stack';
-
-const app = new cdk.App();
-new ChatbotSlackDemoStack(app, 'ChatbotSlackDemoStack');
